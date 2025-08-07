943e259ba49a7528809e29f35769ef21ef9baa4a
Elissa-DI <elissadusabe@gmail.com>
2025-08-07 16:38:19 +0200

    fixed some relations

--- src/prep-station/prep-station.module.ts
+++ src/prep-station/prep-station.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrepStation } from 'src/entities/prepStaion.entity';
 import { PrepStationController } from './prep-station.controller';
 import { PrepStationService } from './prep-station.service';
+import { Restaurant } from 'src/entities';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PrepStation])],
+  imports: [TypeOrmModule.forFeature([PrepStation, Restaurant])],
   controllers: [PrepStationController],
   providers: [PrepStationService],
 })
